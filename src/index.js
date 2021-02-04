bb88af6f94b068cac99c13ed880a726b6fdbffad
Mattias Kjellsson <u.m.kjellsson@gmail.com>
2021-02-04 19:00:25 +0700

    Add service to keep track of things and make things less verbose

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 const express = require('express')
 const Gpio = require('onoff').Gpio
-
+const { pinValue } = require('./service')
 const HIGH_PIN = 17
 const LOW_PIN = 27
 const port = 3000
@@ -17,54 +17,60 @@ app.get('/', (req, res) => {
 
 app.get('/oxygen-high-alarm-off', (req, res) => {
   console.log('Turn off high oxygen alarm')
-  if (highOxygenGPIO.readSync() === 0) {
-    highOxygenGPIO.writeSync(1)
-    console.log(`GPIO ${HIGH_PIN} set to high`)
-  } else {
-    console.log(`GPIO ${HIGH_PIN} already high`)
-  }
-
+  // if (highOxygenGPIO.readSync() === 0) {
+  //   highOxygenGPIO.writeSync(1)
+  //   console.log(`GPIO ${HIGH_PIN} set to high`)
+  // } else {
+  //   console.log(`GPIO ${HIGH_PIN} already high`)
+  // }
+  const result = pinValue(highOxygenGPIO, 1)
+  console.log(result)
   res.send(`Gpio ${HIGH_PIN} set high`)
 })
 
 app.get('oxygen-high-alarm-on', (req, res) => {
   console.log(`Turn on the high oxygen alarm`)
-  if (highOxygenGPIO.readSync() === 1) {
-    highOxygenGPIO.writeSync(0)
-    console.log(`GPIO ${HIGH_PIN} set to low`)
-  } else {
-    console.log(`GPIO ${HIGH_PIN} already low`)
-  }
-
+  // if (highOxygenGPIO.readSync() === 1) {
+  //   highOxygenGPIO.writeSync(0)
+  //   console.log(`GPIO ${HIGH_PIN} set to low`)
+  // } else {
+  //   console.log(`GPIO ${HIGH_PIN} already low`)
+  // }
+  const result = pinValue(highOxygenGPIO, 0)
+  console.log(result)
   res.send(`Gpio ${HIGH_PIN} set low`)
 })
 
 app.get('/oxygen-low-alarm-off', (req, res) => {
   console.log('Turn off high oxygen alarm')
-  if (lowOxygenGPIO.readSync() === 0) {
-    lowOxygenGPIO.writeSync(1)
-    console.log(`GPIO ${LOW_PIN} set to high`)
-  } else {
-    console.log(`GPIO ${LOW_PIN} already high`)
-  }
-
+  // if (lowOxygenGPIO.readSync() === 0) {
+  //   lowOxygenGPIO.writeSync(1)
+  //   console.log(`GPIO ${LOW_PIN} set to high`)
+  // } else {
+  //   console.log(`GPIO ${LOW_PIN} already high`)
+  // }
+  const result = pinValue(lowOxygenGPIO, 1)
+  console.log(result)
   res.send(`Gpio ${LOW_PIN} set high`)
 })
 
 app.get('oxygen-low-alarm-on', (req, res) => {
   console.log(`Turn on the high oxygen alarm`)
-  if (lowOxygenGPIO.readSync() === 1) {
-    lowOxygenGPIO.writeSync(0)
-    console.log(`GPIO ${LOW_PIN} set to low`)
-  } else {
-    console.log(`GPIO ${LOW_PIN} already low`)
-  }
-
+  // if (lowOxygenGPIO.readSync() === 1) {
+  //   lowOxygenGPIO.writeSync(0)
+  //   console.log(`GPIO ${LOW_PIN} set to low`)
+  // } else {
+  //   console.log(`GPIO ${LOW_PIN} already low`)
+  // }
+  const result = pinValue(lowOxygenGPIO, 0)
+  console.log(result)
   res.send(`GPIO ${LOW_PIN} set low`)
 })
 
 app.listen(port, () => {
   console.log(`Listen on port ${port}`)
+  lowOxygenGPIO.write(1, () => { console.log('Low oxygen alarm off')})
+  highOxygenGPIO.write(1, () => { console.log('High oxygen alarm off')})
 })
 
 process.on('SIGINT', _ => {
