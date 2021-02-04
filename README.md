14a788eb431b8212f6f6fe5c10b1f91b8813df8b
Mattias Kjellsson <u.m.kjellsson@gmail.com>
2021-02-04 18:36:48 +0700

    added high and low larm paths and refactored a bit

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
 
 - GPIO 17, green led for high oxygen.
--- When the measured oxygen level is higher than the set high limit
+ When the measured oxygen level is higher than the set high limit
    this GPIO shall go low, that is shut the diode off.
-
--- When the measured value is lower thant the set high limit
+ When the measured value is lower thant the set high limit
    this GPIO shall go high, that is turn the diode on.
 - GPIO 27, red led for low oxygen.
--- When the measuder oxygen level is lower than the set low limit
+ When the measuder oxygen level is lower than the set low limit
    this GPIO shall go low, that is shut the diode off.
-  
--- When the measured value is higher than the set low limit
+ When the measured value is higher than the set low limit
    this GPIO shall go high, that is turn this led on.
--- src/index.js
+++ src/index.js
@@ -1,40 +1,73 @@
+
 const express = require('express')
 const Gpio = require('onoff').Gpio
-const LED = new Gpio(4, 'out')
 
-const app = express()
+const HIGH_PIN = 17
+const LOW_PIN = 27
 const port = 3000
 
+const highOxygenGPIO = new Gpio(HIGH_PIN, 'out')
+const lowOxygenGPIO = new Gpio(LOW_PIN, 'out')
+
+const app = express()
 
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
 
-app.get('/high', (req, res) => {
-  console.log('set gpio high')
-  if (LED.readSync() === 0) {
-    LED.writeSync(1)
-    console.log('Tried and set GPIO 4 high')
+app.get('/oxygen-high-alarm-off', (req, res) => {
+  console.log('Turn off high oxygen alarm')
+  if (highOxygenGPIO.readSync() === 0) {
+    highOxygenGPIO.writeSync(1)
+    console.log(`GPIO ${HIGH_PIN} set to high`)
   } else {
-    console.log('Tried writing high, but ut was already there')
+    console.log(`GPIO ${HIGH_PIN} already high`)
   }
 
-  res.send('Gpio 4 set high')
+  res.send(`Gpio ${HIGH_PIN} set high`)
 })
 
-app.get('/low', (req, res) => {
-  console.log('set gpio low')
-
-  if (LED.readSync() === 1) {
-    LED.writeSync(0)
-    console.log('Tried and set GPIO 4 low')
+app.get('oxygen-high-alarm-on', (req, res) => {
+  console.log(`Turn on the high oxygen alarm`)
+  if (highOxygenGPIO.readSync() === 1) {
+    highOxygenGPIO.writeSync(0)
+    console.log(`GPIO ${HIGH_PIN} set to low`)
   } else {
-    console.log('Tried writing low, but ut was already there')
+    console.log(`GPIO ${HIGH_PIN} already low`)
   }
 
-  res.send('Gpio 4 set low')
+  res.send(`Gpio ${HIGH_PIN} set low`)
+})
+
+app.get('/oxygen-low-alarm-off', (req, res) => {
+  console.log('Turn off high oxygen alarm')
+  if (lowOxygenGPIO.readSync() === 0) {
+    lowOxygenGPIO.writeSync(1)
+    console.log(`GPIO ${LOW_PIN} set to high`)
+  } else {
+    console.log(`GPIO ${LOW_PIN} already high`)
+  }
+
+  res.send(`Gpio ${LOW_PIN} set high`)
+})
+
+app.get('oxygen-low-alarm-on', (req, res) => {
+  console.log(`Turn on the high oxygen alarm`)
+  if (lowOxygenGPIO.readSync() === 1) {
+    lowOxygenGPIO.writeSync(0)
+    console.log(`GPIO ${LOW_PIN} set to low`)
+  } else {
+    console.log(`GPIO ${LOW_PIN} already low`)
+  }
+
+  res.send(`GPIO ${LOW_PIN} set low`)
 })
 
 app.listen(port, () => {
   console.log(`Listen on port ${port}`)
 })
+
+process.on('SIGINT', _ => {
+  lowOxygenGPIO.unexport()
+  highOxygenGPIO.unexport()
+});
