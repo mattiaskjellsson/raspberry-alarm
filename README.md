8d52ea60f1cfb0a7b27c658717e30f507c0d8c98
Mattias Kjellsson <u.m.kjellsson@gmail.com>
2021-02-06 13:58:36 +0800

    Refactor a little... But can't test locally :|

--- README.md
+++ README.md
@@ -9,3 +9,11 @@
    this GPIO shall go low, that is shut the diode off.
  When the measured value is higher than the set low limit
    this GPIO shall go high, that is turn this led on.
+
+
+- Usage.
+
+0. Hook up GPIO pins 17 and 27 to whatever is supposed to be controlled. Nothing to heavy though.
+1. Download the sourcecode
+2. npm install
+3. npm start
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const Gpio = require('onoff').Gpio
+const { highAlarmOff, highAlarmOn, lowAlarmOn, lowAlarmOff} = require('./service')
 
 const HIGH_PIN = 17
 const LOW_PIN = 27
@@ -10,71 +11,28 @@ const lowOxygenGPIO = new Gpio(LOW_PIN, 'out')
 
 const app = express()
 
-const pinValue = (pin, val) => {
-  if (pin.readSync() !== val) {
-    pin.writeSync(val)
-    console.log(`GPIO ${pin} set to ${val}`)
-  } else {
-    console.log(`GPIO ${pin} already set to ${val}`)
-  }
-  
-  return `GPIO ${pin} set to ${val}`
-}
-
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
 
 app.get('/oxygen-high-alarm-off', (req, res) => {
-  console.log('Turn off high oxygen alarm')
-  // if (highOxygenGPIO.readSync() === 0) {
-  //   highOxygenGPIO.writeSync(1)
-  //   console.log(`GPIO ${HIGH_PIN} set to high`)
-  // } else {
-  //   console.log(`GPIO ${HIGH_PIN} already high`)
-  // }
-  const result = pinValue(highOxygenGPIO, 1)
-  console.log(result)
-  res.send(`Gpio ${HIGH_PIN} set high`)
-})
-
-app.get('/oxygen-high-alarm-on', (req, res) => {
-  console.log(`Turn on the high oxygen alarm`)
-  // if (highOxygenGPIO.readSync() === 1) {
-  //   highOxygenGPIO.writeSync(0)
-  //   console.log(`GPIO ${HIGH_PIN} set to low`)
-  // } else {
-  //   console.log(`GPIO ${HIGH_PIN} already low`)
-  // }
-  const result = pinValue(highOxygenGPIO, 0)
-  console.log(result)
-  res.send(`Gpio ${HIGH_PIN} set low`)
+  highAlarmOff()
+  res.send(`High oxygen alarm off`)
 })
 
 app.get('/oxygen-low-alarm-off', (req, res) => {
-  console.log('Turn off high oxygen alarm')
-  // if (lowOxygenGPIO.readSync() === 0) {
-  //   lowOxygenGPIO.writeSync(1)
-  //   console.log(`GPIO ${LOW_PIN} set to high`)
-  // } else {
-  //   console.log(`GPIO ${LOW_PIN} already high`)
-  // }
-  const result = pinValue(lowOxygenGPIO, 1)
-  console.log(result)
-  res.send(`Gpio ${LOW_PIN} set high`)
+  lowAlarmOff()
+  res.send(`Low oxygen alarm off`)
+})
+
+app.get('/oxygen-high-alarm-on', (req, res) => {
+  highAlarmOn()
+  res.send(`High oxygen alarm on`)
 })
 
 app.get('/oxygen-low-alarm-on', (req, res) => {
-  console.log(`Turn on the high oxygen alarm`)
-  // if (lowOxygenGPIO.readSync() === 1) {
-  //   lowOxygenGPIO.writeSync(0)
-  //   console.log(`GPIO ${LOW_PIN} set to low`)
-  // } else {
-  //   console.log(`GPIO ${LOW_PIN} already low`)
-  // }
-  const result = pinValue(lowOxygenGPIO, 0)
-  console.log(result)
-  res.send(`GPIO ${LOW_PIN} set low`)
+  lowAlarmOn()
+  res.send(`Low oxygen alarm on`)
 })
 
 app.listen(port, () => {
